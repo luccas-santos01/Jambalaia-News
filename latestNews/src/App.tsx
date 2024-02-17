fdffb3e35b0d3547cf055363143dfc430f8b270b
Luccas Santos <luccasmsantos@outlook.com>
2024-02-17 16:03:45 -0300

    Processo iniciado

---
+++ latestNews/src/App.tsx
@@ -0,0 +1,14 @@
+import "./App.css";
+import Header from "./components/Header/index.module";
+import LatestNews from "./components/LatestNews/index.module";
+
+function App() {
+  return (
+    <div>
+      <Header />
+      <LatestNews />
+    </div>
+  );
+}
+
+export default App;
